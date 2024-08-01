8b399b615a3b8796569a9720bd9a318d4c0005de
sofian.naidja@gmail.com <163352435+IchibanDZ@users.noreply.github.com>
2024-08-01 13:06:30 +0200

    second

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-
-let inputval = document.querySelector('#inputval');
+let inputval = document.querySelector("#inputval");
+let app = document.querySelector(".app");
 
 async function txtsubmit(val) {
   const reponse = await fetch(`https://api.tvmaze.com/search/shows?q=${val}`);
   const textsubmit = await reponse.json();
-  console.log(textsubmit.val)
-  console.log(textsubmit)
-  };
 
+  textsubmit.map((resultat) => {
+    let container = document.createElement("div");
+    app.appendChild(container);
+    container.className = "container"; 
 
-inputval.addEventListener('keyup', () => {
-console.log(inputval.value)
-txtsubmit(inputval.value) 
-})
+    //afficher image
+    let pic = document.createElement("img");
+    pic.src = resultat.show.image.medium;
+    container.appendChild(pic);
 
+    // afficher titre
+    let title = document.createElement("h2");
+    title.innerText = resultat.show.name;
+    container.appendChild(title);
+  });
+}
 
+inputval.addEventListener("keyup", () => {
+  let recherche = document.getElementById("inputval");
+  let search = recherche.value;
+  txtsubmit(search);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   function removeSpaces(str) {
-//          return str.replace(/\s/g, '');
-//      }
-    
 //      let resultStr = removeSpaces(textsubmit.value);
 //      console.log(resultStr);a tea
 // })
@@ -50,35 +45,31 @@ txtsubmit(inputval.value)
 //   });
 // }
 
-
-
-
 // let container = document.createElement('div');
 
-    // let pic = document.createElement("img");
-    // pic.src = film.miniature;
-    
-    // let title = document.createElement("h2");
-    // title.innerText = film.titre;
-    
-    // let duree = document.createElement("h3");
-    // duree.innerText = film.duree;
-    
-    // let synopsis = document.createElement("h4");
-    // synopsis.innerText = film.synopsis;
-    
-    // let realisateur = document.createElement("h5");
-    // realisateur.innerText = film.realisateur;
-    
-    // let years = document.createElement("h6");
-    // years.innerText = film.annee_de_sortie;
+// let pic = document.createElement("img");
+// pic.src = film.miniature;
 
-    // container.appendChild(pic);
-    // container.appendChild(title);
-    // container.appendChild(duree);
-    // container.appendChild(synopsis);
-    // container.appendChild(realisateur);
-    // container.appendChild(years);
-    
-    // app.appendChild(container);
-   
+// let title = document.createElement("h2");
+// title.innerText = film.titre;
+
+// let duree = document.createElement("h3");
+// duree.innerText = film.duree;
+
+// let synopsis = document.createElement("h4");
+// synopsis.innerText = film.synopsis;
+
+// let realisateur = document.createElement("h5");
+// realisateur.innerText = film.realisateur;
+
+// let years = document.createElement("h6");
+// years.innerText = film.annee_de_sortie;
+
+// container.appendChild(pic);
+// container.appendChild(title);
+// container.appendChild(duree);
+// container.appendChild(synopsis);
+// container.appendChild(realisateur);
+// container.appendChild(years);
+
+// app.appendChild(container);
